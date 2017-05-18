27ff04eeaffb50f0210feede6668dded0849ba18
Eric Yatskowitz <mynameiseric4@gmail.com>
2017-05-18 15:41:00 -0700

    Added much need comments to model.py

--- src/model.py
+++ src/model.py
@@ -12,27 +12,25 @@ import operator
 from pyspark.sql import SparkSession
 from pyspark.mllib.recommendation import ALS
 
+# Initiate SparkSession on local computer
 spark = SparkSession.builder.master('local[4]').getOrCreate()
 
-ratings_df = pd.read_csv('/Users/ericyatskowitz/galvanize_work/MeepleFinder/data/wa_ratings_data.csv')
-ratings_df = ratings_df.rename(columns={'Unnamed: 0':'Username'})
-ratings_df = ratings_df.set_index('Username')
-ratings_df.drop('Unnamed: 1', axis=1, inplace=True)
-ind = []
-for index in ratings_df.index:
-    if ratings_df.loc[index, :].isnull().all() == True:
-        ind.append(index)
-ratings_df.drop(ind, inplace=True)
-ratings_df.fillna(0, inplace=True)
+# Get dictionary to convert board game names to ints
 board_game_index = np.load('/Users/ericyatskowitz/galvanize_work/MeepleFinder/Erics_Web_App/board_game_dict.npy').item()
+# Make dictionary to convert ints to board game names
 board_games = dict((y,x) for x,y in board_game_index.iteritems())
+# Get dictionary to convert usernames to ints
 user_index = np.load('/Users/ericyatskowitz/galvanize_work/MeepleFinder/Erics_Web_App/wa_user_dict.npy').item()
+# Make dictionary to convert ints to usernames
 users = dict((y,x) for x,y in user_index.iteritems())
+# Get ratings data that is ready for sprak ALS model
 als_data = pd.read_csv('/Users/ericyatskowitz/galvanize_work/MeepleFinder/als_ready_wa_ratings_data.csv')
 als_data.drop('Unnamed: 0', axis=1, inplace=True)
 
+# Convert pandas df to spark df
 als_spark_df = spark.createDataFrame(als_data)
 als_spark_df.cache()
+# Make spark ALS model
 als_model = ALS(
     itemCol='board_game',
     userCol='user',
@@ -43,31 +41,41 @@ als_model = ALS(
     maxIter=10
     )
 als_fit_model = als_model.fit(als_spark_df)
+als_fit_model.save('als_model')
 
+# Get board game ranking df to use to make sure predictions are made for every game
 just_ranking_info = pd.read_csv('/Users/ericyatskowitz/galvanize_work/MeepleFinder/data/just_ranking_info.csv')
 just_ranking_info = just_ranking_info.set_index('Title')
+# Create list of tuples for every (user, board game) pair
 predictions_array = list(product(als_data.loc[:, 'user'].unique(), just_ranking_info.index))
+# Convert to pandas df
 predictions_df = pd.DataFrame(predictions_array, columns=['user', 'board_game'])
+# Convert to spark df
 spark_pre_predictions_df = spark.createDataFrame(predictions_df)
 spark_predictions_df = als_fit_model.transform(spark_pre_predictions_df)
 pred_ratings_df = spark_predictions_df.toPandas()
+# Replace any nulls with 0
 pred_ratings_df.fillna(0, inplace=True)
 pred_ratings_df.to_csv('pred_ratings_df.csv')
 
+# Get cleaned board game data with features such as publisher, complexity, etc.
 bg_data_with_dummies = pd.read_csv('model_ready_bg_data.csv')
 bg_data_with_dummies = bg_data_with_dummies.set_index('Title')
+# Rename board game names to match ints use din ALS model
 bg_data_with_dummies_als = bg_data_with_dummies.rename(index=board_game_index)
-x = bg_data_with_dummies_als.values #returns a numpy array
-min_max_scaler = preprocessing.MinMaxScaler()
+x = bg_data_with_dummies_als.values # returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler() # normalizes data
 x_scaled = min_max_scaler.fit_transform(x)
 normalized_als_df = pd.DataFrame(x_scaled,
                                  index=bg_data_with_dummies_als.index,
                                  columns=bg_data_with_dummies_als.columns)
 
+# Make sure there are no games in similarity matrix that there are no ranking data for
 for game in normalized_als_df.index:
     if game not in just_ranking_info.index:
         normalized_als_df.drop(game, inplace=True)
 
+# Make cosine similarity matrix
 Y = pdist(normalized_als_df, 'cosine')
 Y = squareform(Y)
 bg_data_sim = pd.DataFrame(Y, index=normalized_als_df.index, columns=normalized_als_df.index)
